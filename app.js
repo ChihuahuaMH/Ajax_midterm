4bbc7d4686b222fb7a409f6be3a8e893c58f8362
chihuahuaMH <hsien1126@gmail.com>
2020-10-24 00:04:33 +0800

    design UI

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var myRouter = require("./routes/myroute"); // 20200913 Add
 
 var app = express();
 
@@ -21,30 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/my", myRouter);
-
-app.get("/geta", function (req, res) {
-	res.end("Ajax get ");
-});
-
-app.get("/getb", function (req, res) {
-	res.end("getb... ");
-});
-
-app.post("/posta", function (req, res) {
-	res.end("posta... ");
-});
-
-app.get("/getc", function (req, res) {
-	var data = req.query.username;
-	res.end("name: " + data);
-});
-
-app.post("/postb", function (req, res) {
-	var data = req.body.username;
-	res.end("name:" + data);
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
